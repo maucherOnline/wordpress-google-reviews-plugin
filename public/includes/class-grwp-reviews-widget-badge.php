fd2e54eaacdc56e348c9686886fca1b2c20fd073
David Maucher <hallo@maucher-online.com>
2023-03-20 17:36:46 +0100

    sidebar

--- public/includes/class-grwp-reviews-widget-badge.php
+++ public/includes/class-grwp-reviews-widget-badge.php
@@ -45,7 +45,29 @@ class GRWP_Reviews_Widget_Badge
 <?php
         $output .= ob_get_clean();
 		$output .= '</div>';
-        $output .= '<div class="g-review-sidebar right hide"></div>';
+        $output .= '<div class="g-review-sidebar right hide"><div class="grwp-header">'.__("Google Reviews", "grwp");
+        $output .= sprintf('<img src="%s/dist/images/g-stars.svg" alt="" class="g-logo" />', GR_PLUGIN_DIR_URL);
+        $output .= '<span class="grwp-close"></span></div>';
+        $output .= '<div class="grwp-sidebar-inner">';
+
+
+		$google_svg = GR_PLUGIN_DIR_URL . 'dist/images/google-logo-svg.svg';
+
+		$count = 0;
+		foreach ( $this->reviews as $review ) {
+			if ( $max_reviews && is_numeric( $max_reviews ) && intval($max_reviews) <= $count ) {
+				break;
+			}
+			$star_output = $this->get_star_output($review);
+
+			ob_start();
+			require 'partials/grid/markup.php';
+			$output .= ob_get_clean();
+
+			$count++;
+		}
+
+        $output .= '</div></div>';
 
 		return wp_kses( $output, $this->allowed_html );
 
